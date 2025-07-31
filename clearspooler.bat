68475caec807a12cf6efead161f5265f341bcfd6
Ivan Carlos <ivan@ivancarlos.com.br>
2025-07-31 02:27:29 -0300

    Add files via upload

---
+++ clearspooler.bat
@@ -0,0 +1,20 @@
+@echo off
+
+:: Prompt to Run as administrator
+Set "Variable=0" & if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs"
+fsutil dirty query %systemdrive%  >nul 2>&1 && goto :(Privileges_got)
+If "%1"=="%Variable%" (echo. &echo. Please right-click on the file and select &echo. "Run as administrator". &echo. Press any key to exit. &pause>nul 2>&1& exit)
+cmd /u /c echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "%~0", "%Variable%", "", "runas", 1 > "%temp%\getadmin.vbs"&cscript //nologo "%temp%\getadmin.vbs" & exit
+:(Privileges_got)
+
+net stop spooler
+DEL /F /S /Q %systemroot%\System32\spool\PRINTERS\*
+net start spooler
+
+:endSuccess
+	echo Printer Spool tasks completed
+	echo.
+
+:end
+	pause
+	exit
